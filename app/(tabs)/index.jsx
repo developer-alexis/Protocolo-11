f3df9e3550f5c75a2b5bca73fbad7cfd16237a41
Alexis <dev_alexis@outlook.com>
2025-11-17 17:49:40 -0800

    Finished the basic layout of the application

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,6 +1,8 @@
-import { View, Text, StyleSheet, Image, Button, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native'
 import React from 'react'
 import defaultTeamIcon from '@/assets/images/club.png'
+import undoIcon from '@/assets/images/undo.png'
+import resetIcon from '@/assets/images/reset.png'
 
 const index = () => {
   return (
@@ -10,22 +12,26 @@ const index = () => {
       <View style={styles.teamsRow}>
 
         <View style={styles.team}>
-          <Text style={{ textAlign: 'center', fontWeight: 600 }}>Local</Text>
+          <Text style={{ textAlign: 'center', fontWeight: '600' }}>Local</Text>
+
           <Image
             style={styles.teamLogo}
             source={defaultTeamIcon}
           />
+
           <Text style={styles.score}>0</Text>
         </View>
 
         <Text style={styles.vsText}>VS</Text>
 
         <View style={styles.team}>
-          <Text style={{ textAlign: 'center', fontWeight: 600 }}>Visitante</Text>
+          <Text style={{ textAlign: 'center', fontWeight: '600' }}>Visitante</Text>
+
           <Image
             style={styles.teamLogo}
             source={defaultTeamIcon}
           />
+
           <Text style={styles.score}>0</Text>
         </View>
 
@@ -34,8 +40,43 @@ const index = () => {
       <View style={styles.actions}>
         <View style={styles.goals}>
           <Pressable style={styles.goalButtons}>
-            <Text>Marcar gol local</Text>
+            <Text style={styles.goalButtonText}>Marcar gol local</Text>
           </Pressable>
+          <Pressable style={styles.undoGoalButtons}>
+            <Image style={styles.undoGoalIcon} source={undoIcon} />
+          </Pressable>
+        </View>
+
+        <View style={styles.goals}>
+          <Pressable style={styles.goalButtons}>
+            <Text style={styles.goalButtonText}>Marcar gol visitante</Text>
+          </Pressable>
+          <Pressable style={styles.undoGoalButtons}>
+            <Image style={styles.undoGoalIcon} source={undoIcon} />
+          </Pressable>
+        </View>
+
+        <View style={styles.timerActions}>
+          <Pressable style={styles.startTimerButton}>
+            <Text style={{ textAlign: 'center', fontWeight: 600, fontSize: 20 }}>Empezar tiempo</Text>
+          </Pressable>
+          <View style={styles.extraTimerActions}>
+            <Pressable style={styles.addremoveTimeButtons}>
+              <Text style={{ textAlign: 'center', fontWeight: 700 }}>-</Text>
+            </Pressable>
+            <Text style={styles.extraTime}>00:00</Text>
+            <Pressable style={styles.addremoveTimeButtons}>
+              <Text style={{ textAlign: 'center', fontWeight: 700 }}>+</Text>
+            </Pressable>
+          </View>
+          <View style={styles.extraTimerActions}>
+            <Pressable style={styles.confirmExtraTimeButton}>
+              <Text style={{ textAlign: 'center', fontWeight: 700 }}>Añadir tiempo extra</Text>
+            </Pressable>
+            <Pressable style={styles.undoGoalButtons}>
+              <Image style={styles.undoGoalIcon} source={resetIcon} />
+            </Pressable>
+          </View>
         </View>
       </View>
 
@@ -45,51 +86,124 @@ const index = () => {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgb(220, 220, 220)',
+    flex: 1,
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   teamsRow: {
-    display: 'flex',
     flexDirection: 'row',
-    width: '100vw',
-    height: '80vw',
+    width: '100%',
     justifyContent: 'space-around',
     alignItems: 'center',
-    padding: '10px;'
+    padding: 10,
+    marginBottom: 20,
   },
+
   team: {
-    width: '30vw',
+    width: '30%',
     alignItems: 'center',
   },
+
   teamLogo: {
-    width: '120px',
-    height: '120px',
-    marginTop: '10px',
-    objectFit: 'contain',
+    width: 120,
+    height: 120,
+    marginTop: 10,
+    resizeMode: 'contain',
   },
+
   vsText: {
     color: 'red',
-    fontSize: '2.5rem',
-    fontWeight: 900,
+    fontSize: 40,
+    fontWeight: '900',
   },
+
   score: {
-    fontSize: '2.5rem',
-    fontWeight: 900,
+    fontSize: 60,
+    fontWeight: '900',
   },
+
   actions: {
-    padding: '10px',
+    padding: 10,
   },
+
   goals: {
     flexDirection: 'row',
     justifyContent: 'space-around',
+    marginBottom: 30,
   },
+
   goalButtons: {
-    borderRadius: '20px',
-    backgroundColor: 'red',
-  }
-});
+    backgroundColor: 'black',
+    width: 200,
+    height: 40,
+    borderRadius: 10,
+    marginRight: 10,
+    justifyContent: 'center'
+  },
+
+  goalButtonText: {
+    color: 'white',
+    fontWeight: '600',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+
+  undoGoalButtons: {
+    backgroundColor: 'orange',
+    width: 40,
+    height: 40,
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+
+  undoGoalIcon: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+  },
+
+  startTimerButton: {
+    backgroundColor: 'orange',
+    padding: 20,
+    borderRadius: 200,
+  },
+
+  extraTimerActions: {
+    marginTop: 25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
+  extraTime: {
+    backgroundColor: 'black',
+    color: 'white',
+    height: 40,
+    width: 70,
+    borderRadius: 10,
+    textAlign: 'center',
+    marginHorizontal: 10,
+  },
+
+  confirmExtraTimeButton: {
+    backgroundColor: 'orange',
+    textAlign: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    marginRight: 20,
+    borderRadius: 1000
+  },
+
+    addremoveTimeButtons: {
+    backgroundColor: 'orange',
+    width: 40,
+    height: 40,
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+})
 
 export default index
